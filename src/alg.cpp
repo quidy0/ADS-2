e4acdb65b3366709e3c24677e459a0b9b66b95f0
Сергей Букарин <SerBuka2006@yandex.ru>
2024-03-23 15:06:56 +0300

    ADS 2

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,64 +2,64 @@
 #include <cstdint>
 #include "alg.h"
 
-
 double pown(double value, uint16_t n) {
-	double s = value;
-	if (n == 0) {
-		return 1;
-	}
-	for (uint16_t i = 1; i < n; ++i) {
-		s = s * value;
-	}
-	return s;
+    double r = value;
+    if (n == 0) {
+        return 1;
+    }
+    for (uint16_t i = 1; i < n; i++) {
+        r = r * value;
+    }
+    return r;
 }
 
 uint64_t fact(uint16_t n) {
-	uint16_t ret = 1;
-	for (uint16_t i = 1; i <= n; ++i) {
-		ret = i * ret;
-	}
-	return ret;
+    uint64_t r = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        r = i * r;
+    }
+    return r;
 }
 
 double calcItem(double x, uint16_t n) {
-	double result = 1.0;
-
-	for (uint16_t i = 1; i <= n; i++) {
-		result *= x / i;
-	}
-	return result;
+    double r = (pown(x, n) / fact(n));
+    return r;
 }
 
 double expn(double x, uint16_t count) {
-	double sum = 0.0;
-
-	for (uint16_t i = 0; i < count; i++) {
-		sum += calcItem(x, i);
-	}
-	return sum;
+    double r = 1;
+    for (uint16_t i = 1; i <= count; i++) {
+        r += calcItem(x, i);
+    }
+    return r;
 }
 
 double sinn(double x, uint16_t count) {
-	double result = 0;
-	for (int i = 0; i < count; i++) {
-		result += pown(-1, i) * pown(x, 2 * i + 1) / fact(2 * i + 1);
-	}
-	return result;
+    double r = 0;
+    bool flag = false;
+    for (uint16_t i = 1; i < 2 * count; i += 2) {
+        if (flag) {
+            r -= calcItem(x, i);
+            flag = false;
+        } else {
+            r += calcItem(x, i);
+            flag = true;
+        }
+    }
+    return r;
 }
 
 double cosn(double x, uint16_t count) {
-	double result = 1;
-	int sign = -1;
-	double fact = 1;
-	double power = 2;
-
-	for (int i = 1; i < count; i++) {
-		fact *= power * (power - 1);
-		result += sign * pown(x, power) / fact;
-		power += 2;
-		sign *= -1;
-	}
-
-	return result;
+    double r = 1;
+    bool flag = true;
+    for (uint16_t i = 2; i < 2 * count; i += 2) {
+        if (flag) {
+            r -= calcItem(x, i);
+            flag = false;
+        } else {
+            r += calcItem(x, i);
+            flag = true;
+        }
+    }
+    return r;
 }
